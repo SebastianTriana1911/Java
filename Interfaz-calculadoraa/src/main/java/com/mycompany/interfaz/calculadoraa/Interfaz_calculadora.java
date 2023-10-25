6005d272c74c42df710177fbcfc45ce8a0c7a0d1
Sebastian <sebastiantriana1911@gmail.com>
2023-10-25 18:23:43 -0500

    Interfaz calculadora comentado

--- Interfaz-calculadoraa/src/main/java/com/mycompany/interfaz/calculadoraa/Interfaz_calculadora.java
+++ Interfaz-calculadoraa/src/main/java/com/mycompany/interfaz/calculadoraa/Interfaz_calculadora.java
@@ -225,154 +225,302 @@ public class Interfaz_calculadora extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    // Declaracion de variables publicas de clase
+    
+    // Numero1 y numero2 seran las variables que guarden los numeros que ingrese
+    // el usuario
     public int numero1 = 0;
     public int numero2 = 0;
+    
+    // Operacion sera la variable la cual resivira la operacion que desee hacer
+    // el usuario
     public String operacion = "";
     
+    // Multiplicacion
     private void multiplicacionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_multiplicacionActionPerformed
         // TODO add your handling code here:
         
+        // Se extrae lo que haya en la pantalla cuando seleccione la operacion
+        // multiplicacion
         String num1 = pantalla.getText();
+        
+        // numero1 la variable publica de clasa tendra el valor de lo que se
+        // extrajo de la pantalla pero perseado a un entero ya que todo lo que
+        // se devulve de una interdaz son cadenas de texto
         numero1 = Integer.parseInt(num1);
         
+        // La variable publica operacion tendra el valor de multiplicar cuando
+        // seleccione multiplicar, con el fin de poder saber que operacion 
+        // escogio el usuario
         operacion = "*";
         
+        // Se mostrara la pantalla vacia para que el usuario seleccione el 
+        // siguiente numero 
         pantalla.setText("");
-        
     }//GEN-LAST:event_multiplicacionActionPerformed
 
+    // Boton numero 1
     private void num1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num1ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton uno la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 1 que fue el numero que selecciono, esto hara que cada que
+        // oprime 1 se concatene con lo que ya haya en la pantalla
         numero = numero + "1";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num1ActionPerformed
 
+    // Boton numero 2
     private void num2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num2ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton dos la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 2 que fue el numero que selecciono, esto hara que cada que
+        // oprima 2 se concatene con lo que ya haya en la pantalla
         numero = numero + "2";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num2ActionPerformed
 
+    // Boton numero 3
     private void num3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num3ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton tres la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 3 que fue el numero que selecciono, esto hara que cada que
+        // oprima 3 se concatene con lo que ya haya en la pantalla
         numero = numero + "3";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num3ActionPerformed
 
+    // Boton numero 4
     private void num4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num4ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton cuatro la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 4 que fue el numero que selecciono, esto hara que cada que
+        // oprima 4 se concatene con lo que ya haya en la pantalla
         numero = numero + "4";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num4ActionPerformed
 
+    // Boton numero 5
     private void num5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num5ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton cinco la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 5 que fue el numero que selecciono, esto hara que cada que
+        // oprima 5 se concatene con lo que ya haya en la pantalla
         numero = numero + "5";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num5ActionPerformed
 
+    // Boton numero 6
     private void num6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num6ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton seis  la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 6 que fue el numero que selecciono, esto hara que cada que
+        // oprima 6 se concatene con lo que ya haya en la pantalla
         numero = numero + "6";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num6ActionPerformed
 
+    // Boton 7
     private void num7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num7ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton siete  la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 7 que fue el numero que selecciono, esto hara que cada que
+        // oprima 7 se concatene con lo que ya haya en la pantalla
         numero = numero + "7";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num7ActionPerformed
 
+    // Boton numero 8
     private void num8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num8ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton ocho  la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 8 que fue el numero que selecciono, esto hara que cada que
+        // oprima 8 se concatene con lo que ya haya en la pantalla
         numero = numero + "8";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num8ActionPerformed
 
+    // Boton numero 9
     private void num9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num9ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton nueve la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 9 que fue el numero que selecciono, esto hara que cada que
+        // oprima 9 se concatene con lo que ya haya en la pantalla
         numero = numero + "9";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num9ActionPerformed
 
+    // Boton 0
     private void num0ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num0ActionPerformed
         // TODO add your handling code here:
         
+        // Cuando se de click al boton cero la variable numero extraera lo que
+        // haya en pantalla
         String numero = pantalla.getText();
+        
+        // Dicha variable tendra el valor de la misma variable concatenada con
+        // el numero 0 que fue el numero que selecciono, esto hara que cada que
+        // oprima 0 se concatene con lo que ya haya en la pantalla
         numero = numero + "0";
         
+        // Se muestra en la pantalla el nuevo numero ya concatenado 
         pantalla.setText(numero);
     }//GEN-LAST:event_num0ActionPerformed
     
+    // Suma
     private void sumaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sumaActionPerformed
         // TODO add your handling code here:
         
+        // Se extrae lo que haya en la pantalla cuando seleccione la operacion
+        // suma desde el propio boton
         String num1 = pantalla.getText();
+        
+        // numero1 la variable publica de clases, tendra el valor de lo que se
+        // extrajo de la pantalla pero perseado a un entero ya que todo lo que
+        // se devulve de una interdaz son cadenas de texto
         numero1 = Integer.parseInt(num1);
         
+        // La variable publica operacion tendra el valor del signo suma cuando
+        // seleccione el boton sumar, con el fin de poder saber que operacion 
+        // escogio el usuario
         operacion = "+";
         
+        // Se mostrara la pantalla vacia para que el usuario seleccione el 
+        // siguiente numero 
         pantalla.setText("");
         
     }//GEN-LAST:event_sumaActionPerformed
 
+    // Resta
     private void restaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_restaActionPerformed
         // TODO add your handling code here:
         
+        // Se extrae lo que haya en la pantalla cuando seleccione la operacion
+        // resta desde el propio boton
         String num1 = pantalla.getText();
+        
+        // numero1 la variable publica de clases, tendra el valor de lo que se
+        // extrajo de la pantalla pero perseado a un entero ya que todo lo que
+        // se devulve de una interdaz son cadenas de texto
         numero1 = Integer.parseInt(num1);
         
+        // La variable publica operacion tendra el valor del signo resta cuando
+        // seleccione el boton resta, con el fin de poder saber que operacion 
+        // escogio el usuario
         operacion = "-";
         
+        // Se mostrara la pantalla vacia para que el usuario seleccione el 
+        // siguiente numero 
         pantalla.setText("");
         
     }//GEN-LAST:event_restaActionPerformed
 
+    // Division
     private void divisionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_divisionActionPerformed
         // TODO add your handling code here:
         
+        // Se extrae lo que haya en la pantalla cuando seleccione la operacion
+        // dividir desde el propio boton
         String num1 = pantalla.getText();
+        
+        // numero1 la variable publica de clases, tendra el valor de lo que se
+        // extrajo de la pantalla pero perseado a un entero ya que todo lo que
+        // se devulve de una interdaz son cadenas de texto
         numero1 = Integer.parseInt(num1);
         
+        // La variable publica operacion tendra el valor del signo dividir cuando
+        // seleccione el boton division, con el fin de poder saber que operacion 
+        // escogio el usuario
         operacion = "/";
         
+        // Se mostrara la pantalla vacia para que el usuario seleccione el 
+        // siguiente numero 
         pantalla.setText("");
         
     }//GEN-LAST:event_divisionActionPerformed
 
+    // Boton igual - Boton que hace toda las operaciones
     private void igualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_igualActionPerformed
         // TODO add your handling code here:
         
+        // La variable num2 extraera el segundo numero que ingreso el usuario en
+        // pantalla, pues el numero1 se extrajo desde que escogio alguna operacion
+        // para realizar
         String num2 = pantalla.getText();
+        
+        // La variable publica de clase numero2, sera igual al perseo de num2
+        // que es la que contiene el numero convertido en cadena de texto
         numero2 = Integer.parseInt(num2);
       
+        // Se realiza una condicional donde se evalua el valor de operacion,
+        // recordemos que el valor de la operacion se asigna dependiendo del
+        // boton que seleccione el usuario.
+        // Despues de validar que operacion hacer, se asigna a una variable a
+        // dicha operacion, esa variable se persea a un String para asignarselo
+        // a otra y la variable que contiene el resultado ya perseado en tipo
+        // de dato string se muestra en pantalla con set.text
         if (operacion == "+"){
             int resultado = numero1 + numero2;
             String result = Integer.toString(resultado);
